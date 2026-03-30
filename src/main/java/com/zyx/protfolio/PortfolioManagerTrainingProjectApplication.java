f8deafb9d931f3d0b3b257e883051264e0827949
zh0923 <1227440240@qq.com>
2026-03-30 19:09:24 +0800

    项目初始化搭建

---
+++ src/main/java/com/zyx/protfolio/PortfolioManagerTrainingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.zyx.protfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioManagerTrainingProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortfolioManagerTrainingProjectApplication.class, args);
+    }
+
+}
